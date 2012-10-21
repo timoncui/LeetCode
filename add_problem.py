870955fcb26901548a5f0aca91a30ace75a09f76
Timon Cui <timonbaby@163.com>
2012-10-21 14:56:51 -0700

    Add tool to automate creating skeleton cpp file.

---
+++ add_problem.py
@@ -0,0 +1,34 @@
+# Given problem name:
+# 1. Create a proper cpp file from template, exit if file already exist.
+# 2. Output a line for the README.md file
+
+from optparse import OptionParser
+import sys
+import os
+
+if __name__ == '__main__':
+    parser = OptionParser(usage = "Usage: add_problem \"Problem Name\"")
+    (options, args) = parser.parse_args()
+    if len(args) != 1:
+        parser.print_help()
+        sys.exit(1)
+    
+    name = args[0]
+    in_file_name = 'template.txt'
+    out_file_name = name.replace(' ', '_') + '.cpp'
+    if os.path.exists(out_file_name):
+        print out_file_name, 'already exists.'
+        sys.exit(1)
+
+    with open(in_file_name, 'r') as f:
+        s = f.read()
+        s = s.replace('{{ Title }}', name)
+        with open(out_file_name, 'w') as fo:
+            fo.write(s)
+        print 'File', out_file_name, 'created.'
+
+    readme_template = '* [{{ Title }}](https://github.com/timoncui/LeetCode/blob/master/{{ Out file name }})';
+    line = readme_template.replace('{{ Title }}', name)
+    line = line.replace('{{ Out file name }}', out_file_name)
+    print 'Line for README.md:'
+    print line    
