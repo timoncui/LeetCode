71762783cc3d8575b4efb5d2857ff47ed94a9c7e
Timon Cui <timonbaby@163.com>
2012-10-23 22:11:20 -0700

    Add skeleton for ease of debugging.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <stack>
+#include <map>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+  Solution s;
+
+  return 0;
+}
