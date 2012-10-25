06bc1dfec7872467c1368a3a13496cf009568a54
Timon Cui <timonbaby@163.com>
2012-10-25 13:11:32 -0700

    Change letter 'l' to capital to improve readability.

--- Add_Two_Numbers.cpp
+++ Add_Two_Numbers.cpp
@@ -24,12 +24,12 @@ Notes:
  */
 class Solution {
 public:
-  ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+  ListNode *addTwoNumbers(ListNode *L1, ListNode *L2) {
     ListNode *sum = NULL, *tail = NULL;
     int c = 0;
-    while (l1 || l2) {
-      if (l1 == NULL) swap(l1, l2);
-      int s = c + l1->val + (l2 ? l2->val : 0);
+    while (L1 || L2) {
+      if (L1 == NULL) swap(L1, L2);
+      int s = c + L1->val + (L2 ? L2->val : 0);
       if (tail) {
 	tail->next = new ListNode(s % 10);
 	tail = tail->next;
@@ -38,8 +38,8 @@ public:
 	tail = sum;
       }
       c = s / 10;
-      l1 = l1->next;
-      if (l2) l2 = l2->next;
+      L1 = L1->next;
+      if (L2) L2 = L2->next;
     }
     if (c) {
       tail->next = new ListNode(c);
