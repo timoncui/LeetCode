ff0c51ffd644115981eac5eb16fa3c7670a14e40
Timon Cui <timonbaby@163.com>
2012-11-04 15:42:48 -0800

    Add Construct binary tree from pre and in order traversal.

---
+++ Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.cpp
@@ -0,0 +1,45 @@
+/*
+Author: Timon Cui, timonbaby@163.com
+
+Title: Construct Binary Tree from Preorder and Inorder Traversal
+
+Description:
+Given preorder and inorder traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+
+Difficulty rating:
+
+Source:
+http://www.leetcode.com/onlinejudge
+
+Notes:
+
+*/
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  typedef vector<int>::iterator Iter;
+  TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
+    return buildTree(preorder, preorder.begin(), preorder.end(), inorder, inorder.begin(), inorder.end());
+  }
+private:
+  TreeNode *buildTree(vector<int> &preorder, Iter pre_begin, Iter pre_end, vector<int> &inorder, Iter in_begin, Iter in_end) {
+    if (pre_begin == pre_end) return NULL;
+    TreeNode *root = new TreeNode(*pre_begin);
+    Iter in_root = find(in_begin, in_end, root->val);
+    int left_nodes = in_root - in_begin;
+    root->left = buildTree(preorder, pre_begin + 1, pre_begin + 1 + left_nodes, inorder, in_begin, in_root);
+    root->right = buildTree(preorder, pre_begin + 1 + left_nodes, pre_end, inorder, in_root + 1, in_end);
+    return root;
+  }
+};
--- README.md
+++ README.md
@@ -59,6 +59,7 @@
 * [Convert Sorted Array to Binary Search Tree](https://github.com/timoncui/LeetCode/blob/master/Convert_Sorted_Array_to_Binary_Search_Tree.cpp)
 * [Binary Tree Level Order Traversal](https://github.com/timoncui/LeetCode/blob/master/Binary_Tree_Level_Order_Traversal.cpp) `*`
 * [Construct Binary Tree from Inorder and Postorder Traversal](https://github.com/timoncui/LeetCode/blob/master/Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.cpp) `*`
+* [Construct Binary Tree from Preorder and Inorder Traversal](https://github.com/timoncui/LeetCode/blob/master/Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.cpp) `*`
 
 ## Matrix
 * [Maximal Rectangle](https://github.com/timoncui/LeetCode/blob/master/Maximal_Rectangle.cpp) `**`
