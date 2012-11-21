0cafe9f871479629dea9e291786026972cea6880
Timon Cui <timonbaby@163.com>
2012-11-21 15:17:20 -0800

    Shorten the code.

--- Sort_Colors.cpp
+++ Sort_Colors.cpp
@@ -24,16 +24,9 @@ public:
   void sortColors(int A[], int n) {
     int i0 = 0, i2 = n - 1, i = 0;
     while (i <= i2) {
-      if (A[i] == 0) {
-	swap(A[i0], A[i]);
-	i0 ++;
-	i ++;
-      } else if (A[i] == 1) {
-	i ++;
-      } else {
-	swap(A[i], A[i2]);
-	i2 --;
-      }
+      if (A[i] == 0) swap(A[i0 ++], A[i ++]);
+      else if (A[i] == 1) i ++;
+      else swap(A[i], A[i2 --]);
     }
   }
 };
